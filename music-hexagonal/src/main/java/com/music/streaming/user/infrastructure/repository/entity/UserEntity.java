f15aa70ec333ac908501f0da3234ec3b50bc66e0
Lorena Peñas <lorenappff@gmail.com>
2026-05-27 17:50:25 +0200

    add: favouriteSongIds to UserEntity.java as @ElementCollection

--- music-hexagonal/src/main/java/com/music/streaming/user/infrastructure/repository/entity/UserEntity.java
+++ music-hexagonal/src/main/java/com/music/streaming/user/infrastructure/repository/entity/UserEntity.java
@@ -1,12 +1,15 @@
 package com.music.streaming.user.infrastructure.repository.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,4 +23,6 @@ public class UserEntity {
     private String username;
     @Column(nullable = false, unique = true)
     private String email;
+    @ElementCollection
+    private List<String> favouriteSongIds;
 }
