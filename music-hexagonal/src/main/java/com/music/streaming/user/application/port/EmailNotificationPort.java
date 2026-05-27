a06e6f19129fca729f263b5915f71810c899d0c4
Lorena Peñas <lorenappff@gmail.com>
2026-05-27 17:24:23 +0200

    add: notification port

---
+++ music-hexagonal/src/main/java/com/music/streaming/user/application/port/EmailNotificationPort.java
@@ -0,0 +1,7 @@
+package com.music.streaming.user.application.port;
+
+import com.music.streaming.user.domain.User;
+
+public interface EmailNotificationPort {
+    void sendFavoriteLimitReachedEmail(User user);
+}
